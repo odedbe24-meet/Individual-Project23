fb8c2c07daaae17e2fac2a606611c8f638554133
odedbe24-meet <106547160+odedbe24-meet@users.noreply.github.com>
2023-07-26 14:29:56 +0300

    Mid Workig

--- Individual-Project/app.py
+++ Individual-Project/app.py
@@ -58,7 +58,7 @@ def signup():
         try:
             login_session['user'] = auth.create_user_with_email_and_password(email,password)
             UID = login_session['user']['localId']
-            user ={"email": email, "password":password, "username": username}
+            user ={"email": email, "password":password, "username": username, "discovered" : 0, "recommneded": 0}
             db.child("Users").child(UID).set(user)
             
             return render_template("home.html")
@@ -74,7 +74,9 @@ def home():
     if request. method == 'POST':
         print("good")
     else:
-        return render_template("home.html")
+        total_discoverd = db.child("Discoverd").get().val()
+        total_recommended = db.child("Recommended").get().val()
+        return render_template("home.html", total_discoverd=total_discoverd, total_recommended=total_recommended)
 
 
 
@@ -87,6 +89,23 @@ def give():
         print("------------------------------")
         reqs = db.child("Requests").get().val()
         return render_template("give.html", reqs=reqs)
+    else:
+        answer = request.form['answer']
+        giver = db.child("Users").child(login_session['user']['localId']).child("username").get().val() 
+        reqs = reqs = db.child("Requests").get().val()
+        postID = request.form['ID']
+        updated = {answer:giver}
+        db.child("Requests").child(postID).child("Answers").update(updated)
+        answers = db.child("Requests").child(postID).child("Answers").get().val()
+        recommned = db.child("Users").child(login_session['user']["localId"]).child("recommneded").get().val()
+        recommned = recommned +1
+        temp = db.child("Recommended").get().val() +1
+        db.child("Recommended").set(temp)
+        temp = db.child("Discoverd").get().val() + 1
+        db.child("Discoverd").set(temp)
+        db.child("Users").child(login_session['user']["localId"]).child("recommneded").set(recommned)
+
+        return render_template("give.html", reqs=reqs, answers=answers)
     
 
 
@@ -101,7 +120,10 @@ def profile():
         UID = login_session['user']["localId"]
         username = db.child('Users').child(UID).child("username").get().val()
         email = db.child('Users').child(UID).child("email").get().val()
-        return render_template("profile.html", username=username, email=email)
+        discoverd = db.child('Users').child(UID).child("discovered").get().val()
+        recommneded = db.child('Users').child(UID).child("recommneded").get().val()
+        
+        return render_template("profile.html", username=username, email=email, disc=discoverd, recommneded=recommneded)
 
 
 
@@ -117,7 +139,8 @@ def get():
         body = request.form['body']
         UID = login_session['user']['localId']
         username = db.child('Users').child(UID).child('username').get().val()
-        song_request = {"title": title, "body":body, "username":username, "UID":UID }
+        
+        song_request = {"title": title, "body":body, "username":username, "UID":UID, "answers":{} }
         db.child("Requests").push(song_request)
         return redirect(url_for("home"))
 
@@ -130,16 +153,26 @@ def ai():
         num_of_results = 1
         search_word = request.form['search_word']
         results = sp.search(search_word, num_of_results, 0, "track")
+        limit_of_songs = 20
         for i in range(num_of_results):
             print(results['tracks']['items'][i]['id'])
-        recomendation = sp.recommendations(seed_tracks=[results['tracks']['items'][0]['id']],limit=5)
-
+        recomendation = sp.recommendations(seed_tracks=[results['tracks']['items'][0]['id']],limit=limit_of_songs)
+        updated = db.child("Users").child(login_session['user']['localId']).child("discovered").get().val()
+        updated = updated + limit_of_songs
+        db.child("Users").child(login_session['user']['localId']).child("discovered").set(updated)
+        temp = db.child("Discoverd").get().val() + limit_of_songs
+        db.child("Discoverd").set(temp)
         rec_name ={}
+        rec_link =[]
         for track in recomendation['tracks']:
             song_name = track['name']
             artist_name = track['artists'][0]['name']
+            track_id = track['id']
+            spotify_link = "https://open.spotify.com/track/"+track_id
+            print(spotify_link)
+            rec_link.append(spotify_link)
             rec_name[song_name] =artist_name
-        return render_template("ai.html", rec_name=rec_name)
+        return render_template("ai.html", rec_name=rec_name, rec_link=rec_link)
     
 
     else:
